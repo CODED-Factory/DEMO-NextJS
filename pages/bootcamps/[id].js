53a417ff00ed0954527e9038bb2519245496f975
Octowl <alsaff1987@gmail.com>
2020-08-18 13:21:37 +0300

    SSR

--- pages/bootcamps/[id].js
+++ pages/bootcamps/[id].js
@@ -1,33 +1,27 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
-import { useRouter } from "next/router";
 import Error from "next/error";
 
 // Components
 import BootcampDetail from "../../components/BootcampDetail";
 
-export default function BootcampOne() {
-  const { id } = useRouter().query;
-  const [bootcamp, setBootcamp] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const fetchBootcamp = async () => {
-    try {
-      const { data } = await axios.get(`http://localhost:3001/bootcamps/${id}`);
-      setBootcamp(data);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (id) fetchBootcamp();
-  }, [id]);
-
-  if (loading) return <h1>Loading...</h1>;
-
+export default function Bootcamp({ bootcamp }) {
   if (!bootcamp) return <Error statusCode={404} />;
-
   return <BootcampDetail bootcamp={bootcamp} />;
+}
+
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+
+  let bootcamp;
+
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/api/bootcamps/${id}`
+    );
+    bootcamp = data;
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  return { props: { bootcamp } };
 }
--- pages/bootcamps/index.js
+++ pages/bootcamps/index.js
@@ -1,20 +1,18 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
 
 // Components
 import BootcampList from "../../components/BootcampList";
 
-export default function Bootcamps() {
-  const [bootcamps, setBootcamps] = useState([]);
-
-  const fetchBootcamps = async () => {
-    const { data } = await axios.get("http://localhost:3001/bootcamps");
-    setBootcamps(data);
-  };
-
-  useEffect(() => {
-    fetchBootcamps();
-  }, []);
-
+export default function Bootcamps({ bootcamps }) {
   return <BootcampList bootcamps={bootcamps} />;
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios.get("http://localhost:3000/api/bootcamps");
+
+  return {
+    props: {
+      bootcamps: data,
+    },
+  };
 }
